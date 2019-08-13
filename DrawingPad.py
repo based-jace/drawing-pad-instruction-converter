8f7d24322623629d4644e315b6e3eb28919a0a40
Piss <Piss>
2019-08-13 06:23:43 -0500

    Almost done. I just gotta do some maffs.

--- DrawingPad.py
+++ DrawingPad.py
@@ -18,7 +18,7 @@ class ADrawingPad:
         "F0": ("CLR", 0),
         "80": ("PEN", 2),
         "A0": ("C0", 8),
-        "C0": ("MV", -1)
+        "C0": ("MV", 4)
     }
 
     kByteValueBounds = (-8192, 8191)
@@ -46,42 +46,25 @@ class ADrawingPad:
 
         Returns the stringified representation of the parsed commandList
         '''
-        hexCache, commandCache, attemptCache = [], [], []
+
         hexList = [
             hexString[i] + hexString[i+1] 
             for i in range(0, len(hexString), 2)
         ]
-        # TODO: Keep popping until first valid opcode
-        # currentCode = hexList.pop()
+
         currentCode = ""
-        # attempt = 0
-        # print(hexList)
         while len(hexList) > 0:
             hexArgs = []
             currentCode = hexList.pop(0)
             while currentCode not in self.kCodebook:
                 currentCode = hexList.pop(0)
-            if currentCode == "C0":
-                hexCache = copy.deepcopy(hexList)
-                try:
-                    while hexList[0] not in self.kCodebook:
-                        # print(hexArgs)
-                        hexArgs += [hexList.pop(0) for i in range(4)]
-                except:
-                    hexList = hexCache
-                    hexArgs = []
-                    while hexList[0] not in self.kCodebook:
-                        hexArgs.append(hexList.pop(0))
-                    invalidNumOfArgs = len(hexArgs) % 4
-                    # print(invalidNumOfArgs)
-                    for i in range(invalidNumOfArgs):
-                        hexArgs.pop()
-            else:
-                numArgs = self.kCodebook[currentCode][1]
-                # print(numArgs)
-                hexArgs = [hexList.pop(0) for i in range(numArgs)]
-
-
+            try:
+                while hexList[0] not in self.kCodebook:
+                    hexArgs.append(hexList.pop(0))
+            except:
+                # End of list
+                pass
+            
             self.__BuildCommand(currentCode, hexArgs)
 
         self.commandList += self.currentCommandList
@@ -96,6 +79,44 @@ class ADrawingPad:
         listToSend = self.currentCommandList if current else self.commandList
 
         return ";\n".join(listToSend) + ";"
+
+    def __BuildCommand(self, command, hexArgs=[]):
+        '''
+        Sends command and args out to relevant method. 
+        
+        Returns True if successful.
+        '''
+
+        if command == "F0":
+            self.__Clear()
+        else:
+            decodedArgs = self.__InterpretCodes(hexArgs)
+            decodedCommands = self.__RemoveInvalidCodes(command, decodedArgs)
+
+            if decodedCommands != []:
+        
+                if command == "80":
+                    penCommand = decodedCommands[0]
+
+                    self.__SetPenUp(penCommand)
+
+                elif command == "A0":
+                    colorCodes = tuple(decodedCommands)
+                    self.__SetColor(colorCodes)
+
+                elif command == "C0":
+                    numberOfCommands = len(decodedCommands)
+
+                    coordinates = [
+                        (decodedCommands[i], decodedCommands[i+1]) 
+                        for i in range(0, numberOfCommands, 2)
+                    ]
+
+                    print(coordinates)
+
+                    self.__MovePen(coordinates)
+
+        return True
 
     def __Clear(self, sendToCommandList=True):
         '''
@@ -242,41 +263,12 @@ class ADrawingPad:
         elif unweightedY > minMaxValues[1][1]:
             weightedCoordinates[1] = minMaxValues[1][1]
 
+        weightedX = unweightedX - self.currentPoint[0]
+        weightedY = unweightedY - self.currentPoint[1]
+        print(weightedX,weightedY)
+        print(weightedCoordinates)
+
         return tuple(weightedCoordinates)
-
-    def __BuildCommand(self, command, hexArgs=[]):
-        '''
-        Sends command and args out to relevant method. 
-        
-        Returns True if successful.
-        '''
-
-        if command == "F0":
-            self.__Clear()
-        else:
-            decodedCommands = self.__InterpretCodes(hexArgs)
-
-            if command == "80":
-                penCommand = decodedCommands[0]
-                self.__SetPenUp(penCommand)
-
-            elif command == "A0":
-                colorCodes = tuple(decodedCommands)
-                self.__SetColor(colorCodes)
-
-            elif command == "C0":
-                numberOfCommands = len(decodedCommands)
-
-                coordinates = [
-                    (decodedCommands[i], decodedCommands[i+1]) 
-                    for i in range(0, numberOfCommands, 2)
-                ]
-
-                print(coordinates)
-
-                self.__MovePen(coordinates)
-
-        return True
 
     def __InterpretCodes(self, hexCodes):
         ''' 
@@ -285,12 +277,56 @@ class ADrawingPad:
 
         numberOfCodes = len(hexCodes)
 
-        decodedCommands = [
-            self.hexDecConverter.Decode(hexCodes[i], hexCodes[i+1]) 
-            for i in range(0, numberOfCodes, 2)
-        ]
+        decodedCommands = []
 
+        if numberOfCodes % 2 > 0 and numberOfCodes > 0:
+            hexCodes.pop()
+            numberOfCodes = len(hexCodes)
+
+        if numberOfCodes > 0:
+            decodedCommands = [
+                self.hexDecConverter.Decode(hexCodes[i], hexCodes[i+1]) 
+                for i in range(0, numberOfCodes, 2)
+            ]
+        
         return decodedCommands
+
+    def __RemoveInvalidCodes(self, command, decimalArgs):
+        ''' 
+        Removes invalid codes from decimalArgs and returns the updated list.
+
+        If the number of invalid codes is smaller than required for the command,
+        returns an empty list.
+        '''
+
+        numArgsRequired = int(self.kCodebook[command][1] / 2)
+        argListLength = len(decimalArgs)
+
+        if command == "C0":
+            for i, dArg in enumerate(decimalArgs):
+                if not (self.kByteValueBounds[0] <= dArg <= self.kByteValueBounds[1]):
+                    decimalArgs = decimalArgs[:i]
+                    argListLength = len(decimalArgs)
+                    break
+
+            argsTooMany = argListLength % numArgsRequired
+            for i in range(argsTooMany):
+                decimalArgs.pop()
+
+            if len(decimalArgs) < numArgsRequired:
+                decimalArgs = []
+
+        else:
+            argsTooMany = argListLength - numArgsRequired
+            for i in range(argsTooMany):
+                decimalArgs.pop()
+            
+            for dArg in decimalArgs:
+                if not (self.kByteValueBounds[0] <= int(dArg) <= self.kByteValueBounds[1]):
+                    decimalArgs = []
+                    break
+
+        return decimalArgs
 
 
 dp = ADrawingPad()
@@ -317,14 +353,10 @@ dp = ADrawingPad()
 # print(dp.Action("F0A04000417F4000417FC040004000804001C05F205F20804000"))
 
 # Blue Square
-print(dp.Action("F0A040004000417F417FC04000400090400047684F5057384000804001C05F204000400001400140400040007E405B2C4000804000"))
+# print(dp.Action("F0A040004000417F417FC04000400090400047684F5057384000804001C05F204000400001400140400040007E405B2C4000804000"))
 
 # Red Lines
 # print(dp.Action("F0A0417F40004000417FC067086708804001C0670840004000187818784000804000"))
 
 # Orange Lines
-# print(dp.Action("F0A0417F41004000417FC067086708804001C067082C3C18782C3C804000"))
-
-hdc = AHexDecConverter()
-
-# print(hdc.Decode("90", "40"))
+print(dp.Action("F0A0417F41004000417FC067086708804001C067082C3C18782C3C804000"))
